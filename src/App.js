e959d9d0cdd410fce0ffdfa1b42421e9caa36e9c
小蒲 <pulongshan@onerws.com>
2021-06-06 13:16:59 +0800

    1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.scss';
+import 'antd/dist/antd.css';
 import { Card } from "./card/card";
 
 function App() {
--- src/card/card.js
+++ src/card/card.js
@@ -1,16 +1,20 @@
-import { Component } from "react";
+import * as react from "react";
 import "./card.scss";
+import * as icons from '@ant-design/icons';
 
-export class Card extends Component {
+export class Card extends react.Component {
 
+    hideHeader = false;
     render() {
         return (
             <div className='card'>
                 <div className='header'>
                     <span>我是标题</span>
-                    <span></span>
+                    <icons.CaretUpOutlined className='toggle' />
                 </div>
-                <div className='content'></div>
+                <div className='content'>
+                    我是内容 
+                </div>
             </div>
         );
     }
