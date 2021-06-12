a351c9fda35c8618c05acbd97d797ca3c75ed91b
小蒲 <pulongshan@onerws.com>
2021-06-12 12:24:29 +0800

    把项目升级为使用tsx

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.scss';
+import 'antd/dist/antd.css';
+import { Card } from "./card/card";
+
+function App() {
+  return (
+    <Card></Card>
+  );
+}
+
+export default App;
---
+++ src/card/card.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import './card.scss';
+// import *as antd from 'antd';
+// import *as icons from '@ant-design/icons';
+
+export class Card extends React.Component {
+    render() {
+        return (
+            <div>
+            </div>
+        )
+    }
+}
